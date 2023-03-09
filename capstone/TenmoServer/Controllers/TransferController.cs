7ebcbd5432d361438a36d601910d8974eac7c02f
Ben Mullaly <benmullaly14gmail.com>
2023-03-09 11:00:37 -0500

    partway through TransferController

--- capstone/TenmoServer/Controllers/TransferController.cs
+++ capstone/TenmoServer/Controllers/TransferController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
 using TenmoServer.DAO;
+using TenmoServer.Models;
 
 namespace TenmoServer.Controllers
 {
@@ -18,6 +19,43 @@ namespace TenmoServer.Controllers
         }
         //need HttpGet(ID), HttpPost to transfer to other user, HttpPut(TransactionID) to update both account
 
+        [HttpGet("/transfer/user/{userId}")]
+        public ActionResult<IList<Transfer>> GetAllTransfersForUser(int userId)
+        {
+            List<Transfer> transfers = new List<Transfer>();
+
+            transfers = (List<Transfer>)transferDao.GetTransfersByUserId(userId);
+
+            if(transfers.Count != 0)
+            {
+                return transfers;
+            }
+            else
+            {
+                return NoContent();
+            }
+
+        }
+        [HttpGet("/transfer/{transferId}")]
+        public ActionResult<Transfer> GetTransferByTransferId(int transferId)
+        {
+            Transfer transfer = null;
+
+            transfer = transferDao.GetTransfer(transferId);
+
+            if(transfer == null)
+            {
+                return NotFound();
+            } else { return transfer; }
+        }
+
+        [HttpPost()]
+        public ActionResult<Transfer> ExecuteSendTransfer(Transfer transfer)
+        {
+            transferDao.ExecuteTransfer(transfer);
+            return new Transfer();
+        }
+        [HttpPut("{transId")]
 
     }
 
