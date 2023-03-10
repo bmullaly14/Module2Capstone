ed2268b9ecb31450964f5b13f7434a9b7747679d
Kaitlin Zelmanovich <kaitlinmgarman@gmail.com>
2023-03-10 15:25:02 -0500

    client end additions without build errors

--- capstone/TenmoClient/Models/Account.cs
+++ capstone/TenmoClient/Models/Account.cs
@@ -22,5 +22,5 @@ namespace TenmoClient.Models
         }
     }
 }
-}
+
 
--- capstone/TenmoClient/TenmoApp.cs
+++ capstone/TenmoClient/TenmoApp.cs
@@ -73,12 +73,12 @@ namespace TenmoClient
 
             if (menuSelection == 1)
             {
-                ShowBalance(); 
+                //ShowBalance(); 
             }
 
             if (menuSelection == 2)
             {
-                ShowTransfers(); 
+                //ShowTransfers();
                 // View your past transfers
             }
 
@@ -89,13 +89,13 @@ namespace TenmoClient
 
             if (menuSelection == 4)
             {
-                SendTEBucks(); 
+                //SendTEBucks();
                 // Send TE bucks
             }
 
             if (menuSelection == 5)
             {
-                RequestTEBucks(); 
+                //RequestTEBucks(); 
                 // Request TE bucks
             }
 
@@ -161,63 +161,65 @@ namespace TenmoClient
             }
             console.Pause();
         }
+    }
+}
 
-        private void ShowBalance()
-        {
-            try
-            {
-                Account account = tenmoApiService.GetAccount(int accountId);  
-                if (account != null)
-                {
-                    console.PrintBalance(account);  
-                }
-            }
-            catch (Exception ex)
-            {
-                console.PrintError(ex.Message);
-            }
-            console.Pause(); 
-        }
+        //private void ShowBalance()
+        //{
+        //    try
+        //    {
+        //        Account account = tenmoApiService.GetAccount(int accountId);  
+        //        if (account != null)
+        //        {
+        //            console.PrintBalance(account);  
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        console.PrintError(ex.Message);
+        //    }
+        //    console.Pause(); 
+        //}
 
 
 
-        private void ShowTransfers()
-        {
-            try
-            {
+        //private void ShowTransfers()
+        //{
+        //    try
+        //    {
                 //int accountId = console.PromptForInteger("Please enter an account number", 0);
                 //if (accountId == 0)
                 //{
                 //    return;
                 //}
-                Account account = tenmoApiService.GetTransferHistory();
-                console.PrintSuccess($" Your account {account} balance is ");
+            //    Account account = tenmoApiService.GetTransferHistory();
+            //    console.PrintSuccess($" Your account {account} balance is ");
 
-            }
-            catch (Exception ex)
-            {
-                console.PrintError(ex.Message);
-            }
-            console.Pause();
-        }
+            //}
+            //catch (Exception ex)
+            //{
+            //    console.PrintError(ex.Message);
+            //}
+            //console.Pause();
+//        }
 
-        private void SendTEBucks()
-        {
-            try
-            {
-                int accountID = console.PromptForInteger("Please enter the account number you would like to send $ to."); 
-                if(accountID == 0)
-                {
-                    return;
-                }
-                Account account = tenmoApiService.GetAccount(accountID);
-                Console.PrintSuccess();
-            }
-            catch (Exception ex)
-            {
-                console.PrintError(ex.Message);
-            }
-            console.Pause();
-        }
-    }
-}
+//        private void SendTEBucks()
+//        {
+//            try
+//            {
+//                int accountID = console.PromptForInteger("Please enter the account number you would like to send $ to."); 
+//                if(accountID == 0)
+//                {
+//                    return;
+//                }
+//                Account account = tenmoApiService.GetAccount(accountID);
+//                Console.PrintSuccess();
+//            }
+//            catch (Exception ex)
+//            {
+//                console.PrintError(ex.Message);
+//            }
+//            console.Pause();
+//        }
+//    }
+//}
